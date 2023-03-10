da29eba7009e7b4bf3cf22ef82c5adbfb262d652
tod619 <trevorodwyer619@hotmail.com>
2023-03-10 20:40:15 +0000

    add/remove styles to form-container on click in style.css file

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ const signupBtn = document.querySelector('.signup-btn')
 const loginBtn = document.querySelector('.login-btn')
 const signupXBtn = document.querySelector('.signup-x')
 const loginXbtn = document.querySelector('.login-x')
+const formContainer = document.querySelector('.form-container')
 
 signupBtn.addEventListener('click', () => {
     signupModal.classList.add('display')
+    formContainer.classList.add('display')
 })
 
 signupXBtn.addEventListener('click', () => {
     signupModal.classList.remove('display')
+    formContainer.classList.remove('display')
 })
 
 loginBtn.addEventListener('click', () => {
     loginModal.classList.add('display')
+    formContainer.classList.add('display')
 })
 
 loginXbtn.addEventListener('click', () => {
     loginModal.classList.remove('display')
+    formContainer.classList.remove('display')
 })
