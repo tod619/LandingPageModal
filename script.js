c459d2cfbe27d70560833c7be37821b34f30fc9d
tod619 <trevorodwyer619@hotmail.com>
2023-03-10 20:30:07 +0000

    Show/hide modals on click in script.js file

---
+++ script.js
@@ -0,0 +1,22 @@
+const signupModal = document.querySelector('.signup-form-wrapper')
+const loginModal = document.querySelector('.login-form-wrapper')
+const signupBtn = document.querySelector('.signup-btn')
+const loginBtn = document.querySelector('.login-btn')
+const signupXBtn = document.querySelector('.signup-x')
+const loginXbtn = document.querySelector('.login-x')
+
+signupBtn.addEventListener('click', () => {
+    signupModal.classList.add('display')
+})
+
+signupXBtn.addEventListener('click', () => {
+    signupModal.classList.remove('display')
+})
+
+loginBtn.addEventListener('click', () => {
+    loginModal.classList.add('display')
+})
+
+loginXbtn.addEventListener('click', () => {
+    loginModal.classList.remove('display')
+})
